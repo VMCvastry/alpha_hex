ebb758ab3f506eea735cb3481289540255d88f93
vmcvastry <009valerio@gmail.com>
2022-04-12 11:20:12 +0200

    .
    Took 19 minutes

--- tests.py
+++ tests.py
@@ -12,12 +12,14 @@ from torch.utils.data import DataLoader, TensorDataset
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 logging.basicConfig(level=logging.INFO)
-
-model = NET(2, HIDDEN_FEATURES, RESNET_DEPTH, VALUE_HEAD_SIZE).to(device)
-loss_fn = nn.MSELoss(reduction="mean")
-optimizer = optim.Adam(model.parameters(), lr=LEARNING_RATE, weight_decay=WEIGHT_DECAY)
-trainer = Trainer(model=model, loss_fn=loss_fn, optimizer=optimizer)
-game = Game([[1, None, None], [1, -1, None], [-1, 1, -1]])
+#
+# model = NET(2, HIDDEN_FEATURES, RESNET_DEPTH, VALUE_HEAD_SIZE).to(device)
+# loss_fn = nn.MSELoss(reduction="mean")
+# optimizer = optim.Adam(model.parameters(), lr=LEARNING_RATE, weight_decay=WEIGHT_DECAY)
+trainer = Trainer(model_name="NET_2022-04-12_01-32-12")
+game = Game([[1, 0, 0], [1, -1, 0], [-1, 1, -1]])
+print(game)
+print(trainer.poll(game.get_state()))
 # game = Game([[None, None, None], [1, -1, None], [None, 1, -1]])
 # game = Game()
 
