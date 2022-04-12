8227416d8860e2e6fe2aeeeac1413b5c795d4ebc
server <009valerio@gmail.com>
2022-04-12 12:14:36 +0200

    automate increasing gen

--- automate.py
+++ automate.py
@@ -4,6 +4,7 @@ from self_play import run_self_play
 model_name = ""
 gen = 1
 while 1:
-    run_self_play("gen1", model_name)
-    model_name = train_net("gen1", model_name)
+    run_self_play(f"gen{gen}", model_name)
+    model_name = train_net(f"gen{gen}", model_name)
     gen += 1
+    print(f"GEN: {gen}")
