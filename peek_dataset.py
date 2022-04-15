5cc26cae4daea4ed2b107c073cf4a7a6cfc9ea45
server <009valerio@gmail.com>
2022-04-15 17:30:53 +0200

    check dataset real size

--- peek_dataset.py
+++ peek_dataset.py
@@ -1,6 +1,13 @@
+import torch
+
 from torch.utils.data import DataLoader
 
 from custom_dataset import CustomDataset
+import random
+test_loader_one = CustomDataset.load("./training_data", "gen10")
+# a=list(test_loader_one)
+# random.shuffle(a)
+s=[]
 
 test_loader_one = CustomDataset.load("./training_data", "gen1")
 test_loader_one = DataLoader(test_loader_one, batch_size=1, shuffle=True)
@@ -12,3 +19,16 @@ for samples, targets1, targets2 in test_loader_one:
     print(targets1)
     print(targets2)
     input("continue")
+    s.append(samples)
+print(len(s))
+
+unique=[]
+for ss in s:
+    flag = True
+    for sss in unique:
+        if torch.equal(ss,sss):
+            flag=False
+            break
+    if flag:
+        unique.append(ss)
+print(len(unique))
