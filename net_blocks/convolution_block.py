batch normalization fix

net_blocks/convolution_block.py
@@ -26,5 +26,5 @@ class Convolution(nn.Module):
         """
         x = self.input_conv(x)
         s = self.batch_norm(x)
-        x = F.relu(x)
+        x = F.relu(s)
         return x

net_blocks/policy_head.py
@@ -20,7 +20,7 @@ class PolicyHead(nn.Module):
     ) -> torch.Tensor:
         x = self.conv(x)
         s = self.batch_norm(x)
-        x = F.relu(x)
+        x = F.relu(s)
         x = x.reshape((-1, 18))  # double grid to vector
         x = self.fcl(x)
         x = F.softmax(x, dim=1)  # to get probability distribution

net_blocks/value_head.py
@@ -18,7 +18,7 @@ class ValueHead(nn.Module):
     ) -> torch.Tensor:
         x = self.conv(x)
         s = self.batch_norm(x)
-        x = F.relu(x)
+        x = F.relu(s)
         x = x.reshape((-1, 9))  # grid to vector
         x = self.fcl1(x)
         x = F.relu(x)