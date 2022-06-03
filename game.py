e471187b6dadeee4e9b7ea31068efea636c259b3
vmcvastry <009valerio@gmail.com>
2022-06-03 20:09:24 +0200

    start
    Took 17 minutes

--- game.py
+++ game.py
@@ -2,11 +2,13 @@ from __future__ import annotations
 
 import copy
 
+N = 6
+
 
 class Game:
     def __init__(self, board: list = None, player=1):
         if board is None:
-            self.board = [[0] * 3 for i in range(3)]
+            self.board = [[0] * N for i in range(N)]
         else:
             self.board = copy.deepcopy(board)
         self.player = player
@@ -32,34 +34,22 @@ class Game:
     def set_mark(self, move: Move):
         assert move.mark == self.player
         self.board[move.x][move.y] = move.mark
-        winner = self.check_tic_tac_toe()
+        winner = self.check_if_winner()
         if winner is not None:
             # logging.debug("{} wins!".format(winner))
             return winner
         self.player = -1 * self.player
 
-    def check_tic_tac_toe(self) -> int | None:
-        # Check rows
-        for i in range(3):
-            if self.board[i][0] == self.board[i][1] == self.board[i][2] != 0:
-                return self.board[i][0]
-        # Check columns
-        for i in range(3):
-            if self.board[0][i] == self.board[1][i] == self.board[2][i] != 0:
-                return self.board[0][i]
-        # Check diagonals
-        if self.board[0][0] == self.board[1][1] == self.board[2][2] != 0:
-            return self.board[0][0]
-        if self.board[0][2] == self.board[1][1] == self.board[2][0] != 0:
-            return self.board[0][2]
-        if all((self.board[x][y] != 0 for x in range(3) for y in range(3))):
-            return 0
+    def check_if_winner(self) -> int | None:
+
+        if all((self.board[x][y] != 0 for x in range(N) for y in range(N))):
+            return self.player  # the player placing the last stone wins
         return None
 
     def get_available_moves(self) -> set[Game.Move]:
         moves = set()
-        for i in range(3):
-            for j in range(3):
+        for i in range(N):
+            for j in range(N):
                 if self.board[i][j] == 0:
                     moves.add(Game.Move(i, j, self.player))
         return moves
@@ -69,8 +59,8 @@ class Game:
             "\n"
             + "\n".join(
                 [
-                    str([self.board[x][y] if self.board[x][y] else 0 for y in range(3)])
-                    for x in range(3)
+                    str([self.board[x][y] if self.board[x][y] else 0 for y in range(N)])
+                    for x in range(N)
                 ]
             )
             + "\n"
