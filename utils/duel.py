b6b8e1d7baa84ae6887aadb6e9e6da3b1d4fb219
vmcvastry <009valerio@gmail.com>
2022-12-26 17:34:45 +0100

    Random choice
    Took 59 minutes

--- utils/duel.py
+++ utils/duel.py
@@ -21,20 +21,14 @@ def duel(trainer1: Trainer, trainer2: Trainer):
     game = Game()
     while 1:
         player = MCTS(
-            trainer1,
-            game.get_state(),
-            game.player,
-            exploration=1.4,
+            trainer1, game.get_state(), game.player, exploration=1.4, temperature=1
         )
         move, _ = player.search()
         res = turn(game, move)
         if res is not None:
             return res
         player = MCTS(
-            trainer2,
-            game.get_state(),
-            game.player,
-            exploration=1.4,
+            trainer2, game.get_state(), game.player, exploration=1.4, temperature=1
         )
         move, _ = player.search()
         res = turn(game, move)
